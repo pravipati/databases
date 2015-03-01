562c024e3f0065b838352268245c3cb822e8434b
pravipati <psravipati@gmail.com>
2015-02-28 17:34:11 -0800

    Sequelize refactor started

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,41 @@
-var models = require('../models');
+// var models = require('../models');
+// var bluebird = require('bluebird');
+
+
+
+// module.exports = {
+//   messages: {
+//     get: function (req, res) {
+//       //username, message, roomname
+//       // console.log(res);
+//       models.messages.get(req, res);
+//     }, // a function which handles a get request for all messages
+//     post: function (req, res) {
+//       var body = "";
+
+//       models.messages.post(req);
+//       res.status(201).send();
+//     } // a function which handles posting a message to the database
+//   },
+
+//   users: {
+//     // Ditto as above
+//     get: function (req, res) {
+//       models.users.get(req, res);
+//     },
+//     post: function (req, res) {
+//       models.users.post(req, res);
+//     }
+//   }
+// };
+
+
 var bluebird = require('bluebird');
+var db = require('./db');
 
 
+// db.Message.create(values) -> creates message instance and saves it
+// findall, findOrCreate,
 
 module.exports = {
   messages: {
--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,56 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-// Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
-// and to the database "chat".
+// // Create a database connection and export it from this file.
+// // You will need to connect with the user "root", no password,
+// // and to the database "chat".
 
 
-exports.dbConnection = mysql.createConnection({
-      user: "root",
-      password: "",
-      database: "superduperchat4"
-    });
-exports.dbConnection.connect();
+// exports.dbConnection = mysql.createConnection({
+//       user: "root",
+//       password: "",
+//       database: "superduperchat4"
+//     });
+// exports.dbConnection.connect();
+
+
+var sequelize = require('sequelize');
+
+/* You'll need to
+ * npm install sequelize
+ * before running this example. Documentation is at http://sequelizejs.com/
+ */
+
+var Sequelize = require("sequelize");
+var orm = new Sequelize("superduperchat4", "root", "");
+/* TODO this constructor takes the database name, username, then password.
+ * Modify the arguments if you need to */
+
+/* first define the data structure by giving property names and datatypes
+ * See http://sequelizejs.com for other datatypes you can use besides STRING. */
+var User = orm.define('User', {
+  username: Sequelize.STRING,
+  id: Sequelize.INTEGER
+});
+
+var Message = orm.define('Message' {
+  text: Sequelize.STRING,
+  roomname: Sequelize.STRING
+});
+
+//creates one to many association between user and messages
+User.hasMany(Message);
+
+//creates association between message and user, adds the userID to the message table
+Message.belongsTo(User);
+
+//creates user table
+User.sync();
+
+//creates message table
+Message.sync();
+
+exports.User = User;
+exports.Message = Message;
+
+
+
